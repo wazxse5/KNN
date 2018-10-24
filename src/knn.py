f33b615fab669bf066ee5e8d0abe4614bdb681fc
ShadowBr0 <pakerns221@gmail.com>
2018-10-24 22:06:27 +0200

    predikt ktory zwraca tablice 3 wymiarowa: tablica[ (tablica {nazwa kwiatu}), (tablica...)] - zawiera etykiety najblizszych punktow

--- src/knn.py
+++ src/knn.py
@@ -19,11 +19,17 @@ class knn:
     def predict(self, list):
        for i in list:
            distances = []
+           neightbours = []
            for j in self.list:
                 distances.append((j,distance.euclidean(i[0:4], j[0:4])))
-       distances.sort(key=take2nd)
-       printarray(distances)
-    def score(self, list):
+           distances.sort(key=take2nd)
+           #printarray(distances)
+           for x in range(self.k):
+                neightbours.append(distances[x][0][4])
+           print(neightbours)
+       return neightbours
+
+    def score(self, list, labels):
         result = 0.0
         return result
 
@@ -31,6 +37,7 @@ class knn:
 
 arraytest = np.array(pd.read_csv('../resources/test_data.csv', sep=',', header=None))
 arraylearn = np.array(pd.read_csv('../resources/learn_data.csv', sep=',', header=None))
-kn = knn(arraylearn,5)
-kn.predict(arraytest)
+kn = knn(arraylearn,15)
+sc = kn.predict(arraytest)
+wsp = kn.score(arraytest,sc)
 #print(kn.list)
