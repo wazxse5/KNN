9bf3f8b2292c2d547e33b5d7187c607646a495f5
wazxse5 <wazxse5@gmail.com>
2018-10-24 15:01:34 +0200

    reading data from csv files resources added

--- src/knn.py
+++ src/knn.py
@@ -1,10 +1,23 @@
+from scipy.spatial.distance import euclidean
+
+
 class knn:
 
-    def __init__(self, list):
+    def __init__(self, list, k):
         self.list = list
+        self.k = k
 
     def predict(self, list):
-        return list
+        test = ()
+        for x in list:
+            print(x)
+           # test[x] = x[0:3]
+        print(test)
+        for i in list:
+            nearest = ()
+            for j in self.list:
+               # distance = euclidean(list[3:4][i], self.list[3:4][j])
+                print(j)
 
     def score(self, list):
         result = 0.0
