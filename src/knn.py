b4b1d72ce82136f9e18e54290d34d2ed83d13134
wazxse5 <wazxse5@gmail.com>
2018-10-24 10:02:38 +0200

    initial commit

---
+++ src/knn.py
@@ -0,0 +1,11 @@
+class knn:
+
+    def __init__(self, list):
+        self.list = list
+
+    def predict(self, list):
+        return list
+
+    def score(self, list):
+        result = 0.0
+        return result
