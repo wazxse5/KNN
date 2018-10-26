3ada484bfc51cbb690bc81d56c403b0abe1ab04d
wazxse5 <wazxse5@gmail.com>
2018-10-26 19:57:13 +0200

    final commit

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ import pandas as pd
 from src.functions import remove_labels_from
 from src.knn import Knn
 
-array_test = np.array(pd.read_csv('resources/test_data.csv', sep=',', header=None))
-array_learn = np.array(pd.read_csv('resources/learn_data.csv', sep=',', header=None))
+array_test = np.array(pd.read_csv('../resources/test_data.csv', sep=',', header=None))
+array_learn = np.array(pd.read_csv('../resources/learn_data.csv', sep=',', header=None))
+# array_test = np.array(pd.read_csv('resources/test_data.csv', sep=',', header=None))
+# array_learn = np.array(pd.read_csv('resources/learn_data.csv', sep=',', header=None))
 
 kn = Knn(array_learn, 15)
 sc = kn.predict(remove_labels_from(array_test))
